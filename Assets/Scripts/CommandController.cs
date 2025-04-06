339f9d25512a0af3443911d7a62dd44b7ae667c5
Lavinia Lehaci <lh.lavinia98@gmail.com>
2025-04-06 15:02:38 +0200

    Add default command to save console history to file

--- Assets/Scripts/CommandController.cs
+++ Assets/Scripts/CommandController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 [ExecuteInEditMode]
 public class CommandController : MonoBehaviour
@@ -19,37 +20,19 @@ public class CommandController : MonoBehaviour
     private bool _displayConsole;
     private Vector2 _scroll;
     private string _commandInput;
-    private const int _maxCommandHistorySize = 100;
+    private const int _maxCommandConsoleSize = 100;
 
     private List<object> _commandList = new List<object>();
-    private List<string> _helpCommands = new List<string>();
     private Queue<string> _consoleHistory = new Queue<string>();
+    private Queue<string> _logs = new Queue<string>();
 
     public void Start()
     {
         _consoleHistory.Enqueue("Type 'help' to see all commands.");
-        UnityEvent help = new UnityEvent();
-        help.AddListener(() => {   
-            foreach(string str in _helpCommands)
-            {
-                _consoleHistory.Enqueue(str);
-            }} 
-        );
-        _commandList.Add(new Command("help", "display all commands", help));
-        _helpCommands.Add("help - display all commands");
+        _logs.Enqueue("Type 'help' to see all commands.");
 
-        UnityEvent clear = new UnityEvent();
-        clear.AddListener(() => { 
-            _consoleHistory.Clear();
-        });
-        _commandList.Add(new Command("clear", "clear console", clear));
-        _helpCommands.Add("clear - clear console");
-
-        foreach (CommandDetails command in commands)
-        {
-            _commandList.Add(new Command<string>(command.name, command.description, command.function));
-            _helpCommands.Add($"{command.name} - {command.description}");
-        }
+        RegisterDefaultCommands();
+        AddCommandsFromInspector();
     }
 
     public void Update()
@@ -57,33 +40,89 @@ public class CommandController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.BackQuote))
             _displayConsole = !_displayConsole;
 
-        if(_consoleHistory.Count > _maxCommandHistorySize)
+        if(_consoleHistory.Count > _maxCommandConsoleSize)
             _consoleHistory.Dequeue();
+
+        if(_consoleHistory.Count == 0)
+            _consoleHistory.Enqueue("Type 'help' to see all commands.");
     }
 
-    public void PrintWithArgs(string args)
+    private void RegisterDefaultCommands()
     {
-        string print = "";
-        for(int i = 1; i < args.Split(' ').Length; i++)
+        UnityEvent showHelpEvent = new UnityEvent();
+        showHelpEvent.AddListener(() => {   
+            for(int i = 0; i < _commandList.Count; i++)
+            {
+                CommandBase command = _commandList[i] as CommandBase;
+                string str = $"{command.commandName} - {command.commandDescription}";
+                _consoleHistory.Enqueue(str);
+                _logs.Enqueue(str);
+            }
+            } 
+        );
+        Command showHelpCommand = new Command("help", "display all commands", showHelpEvent);
+        _commandList.Add(showHelpCommand);
+
+        UnityEvent clearEvent = new UnityEvent();
+        clearEvent.AddListener(() => { _consoleHistory.Clear(); });
+        Command clearCommand = new Command("clear", "clear console (does not delete console history)", clearEvent);
+        _commandList.Add(clearCommand);
+
+        UnityEvent<string> saveLogsEvent = new UnityEvent<string>();
+        saveLogsEvent.AddListener(WriteLogsToFile);
+        Command<string> saveLogsCommand = new Command<string>("save_logs", "save the console history to a file", saveLogsEvent);
+        _commandList.Add(saveLogsCommand);
+    }
+
+    private void AddCommandsFromInspector()
+    {
+        foreach (CommandDetails command in commands)
         {
-            print += args.Split(' ')[i] + " ";
+            _commandList.Add(new Command<string>(command.name, command.description, command.function));
         }
-        Debug.Log(print);
     }
 
-    public void PrintNoArgs()
+    public void WriteLogsToFile(string args)
     {
-        Debug.Log("Simple print, no args.");
-    }
+        string folderPath = "Assets/Logs";    
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
 
-    public void PrintWarning()
-    {
-        Debug.LogWarning("A warning assigned to this transform!", transform);
+        string fileName = "";
+        string[] argsArray = args.Split(' ');
+        if(argsArray.Length == 1)
+            fileName = "output";
+        else
+        {
+            for(int i = 1; i < argsArray.Length; i++)
+            {
+                fileName += argsArray[i];
+            }
+        }
+        string filePath = Path.Combine(folderPath, fileName + ".txt");
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true)) 
+            {
+                foreach(string log in _logs)
+                {
+                    writer.WriteLine(log);
+                }
+            }
+
+            Debug.Log($"Logs written to {filePath}");
+        }
+        catch(System.Exception ex)
+        {
+            Debug.LogWarning($"Failed to save logs to file. \nError: {ex.Message} \nStackTrace: {ex.StackTrace}");
+        }
     }
 
     public void AddLog(string str)
     {
         _consoleHistory.Enqueue(str);
+        _logs.Enqueue(str);
     }
 
     private void OnGUI()
@@ -120,7 +159,18 @@ public class CommandController : MonoBehaviour
             foreach(string elem in _consoleHistory)
             {
                 float labelHeight = wrapStyle.CalcHeight(new GUIContent(elem), Screen.width * 0.98f);
-                GUI.Label(new Rect(Screen.width * 0.01f, yOffset, Screen.width * 0.98f, labelHeight), elem);
+
+                if(elem == _consoleHistory.Peek())
+                {
+                    GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+                    labelStyle.fontStyle = FontStyle.Italic; 
+                    GUI.Label(new Rect(Screen.width * 0.01f, yOffset, Screen.width * 0.98f, labelHeight), elem, labelStyle);
+                }
+                else
+                { 
+                    GUI.Label(new Rect(Screen.width * 0.01f, yOffset, Screen.width * 0.98f, labelHeight), elem);
+                }
+
                 yOffset += labelHeight;
             }
         }
@@ -134,12 +184,14 @@ public class CommandController : MonoBehaviour
 
         if(Event.current.keyCode == KeyCode.Return)
         {
-            if(_commandInput != "")
-                _consoleHistory.Enqueue("> " + _commandInput);
+            if(String.IsNullOrEmpty(_commandInput))
+                return;
 
+            _consoleHistory.Enqueue("> " + _commandInput);
+            _logs.Enqueue($"> {_commandInput}");
+            
             HandleCommandInput();
             _commandInput = "";
-
             _scroll.y = float.MaxValue;
         }
     }
@@ -161,5 +213,27 @@ public class CommandController : MonoBehaviour
                 }
             }
         }
+    }
+
+    // Testing commands
+    public void PrintWithArgs(string args)
+    {
+        string print = "";
+        string[] argsArray = args.Split(' ');
+        for(int i = 1; i < argsArray.Length; i++)
+        {
+            print += argsArray[i] + " ";
+        }
+        Debug.Log(print);
+    }
+
+    public void PrintNoArgs()
+    {
+        Debug.Log("Simple print, no args.");
+    }
+
+    public void PrintWarning()
+    {
+        Debug.LogWarning($"A warning assigned to this transform! {transform}");
     }
 }
